81fda3a60e320f1a0f2107a3a2e185b7845710bc
Arpit Gupta <gupta.14arpit@gmail.com>
2025-05-06 23:11:30 +0545

    Integrate: Backend search, sort, fitler, pagination with frontend integrated

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,68 +1,93 @@
-import React from 'react';
-import { FaCartPlus } from 'react-icons/fa';
-import { MdChecklistRtl } from 'react-icons/md';
-import { useNavigate } from 'react-router-dom';
+import React from "react";
+import { FaCartPlus } from "react-icons/fa";
+import { MdChecklistRtl } from "react-icons/md";
+import { useNavigate } from "react-router-dom";
 
 const ProductCard = ({ book }) => {
-  const {
-    id,
-    imageurls,
-    title,
-    price,
-    discountedPrice,
-    rating,
-    numberOfReviews,
-  } = book;
+	const {
+		id,
+		imageurls,
+		title,
+		price,
+		discountedPrice,
+		rating,
+		numberOfReviews,
+	} = book;
 
-  const navigate = useNavigate();
-  return (
-    <div onClick={() => navigate(`/book/${id}`)}
-    className="transition-transform transform hover:scale-102 hover:cursor-pointer duration-200 border rounded-lg shadow-md p-4 bg-white hover:bg-gray-50 w-[250px]">
-      {/* Product Image */}
-      <img
-        src={imageurls}
-        alt={title}
-        className="w-full h-40 object-cover rounded-md mb-3 transition-all duration-200 hover:opacity-80"
-      />
+	const {
+		bookId,
+		bookTitle,
+		bookISBN,
+		bookPrice,
+		bookFinalPrice,
+		bookReviewCount,
+		bookRating,
+		authorName,
+		publicationName,
+		genres,
+	} = book;
 
-      {/* Product Title */}
-      <h2 className="text-sm font-semibold text-gray-900 truncate hover:text-[#359BA2]">{title}</h2>
+	const navigate = useNavigate();
+	return (
+		<div
+			onClick={() => navigate(`/Book/${bookId}`)}
+			className="transition-transform transform hover:scale-102 hover:cursor-pointer duration-200 border rounded-lg shadow-md p-4 bg-white hover:bg-gray-50 w-[250px]"
+		>
+			{/* Product Image */}
+			<img
+				src={bookTitle}
+				alt={bookTitle}
+				className="w-full h-40 object-cover rounded-md mb-3 transition-all duration-200 hover:opacity-80"
+			/>
 
-      {/* Rating */}
-      <div className="flex items-center text-xs text-yellow-500 mt-1">
-        ⭐ {rating}
-        <span className="text-gray-500 ml-1">({numberOfReviews} reviews)</span>
-      </div>
+			{/* Product Title */}
+			<h2 className="text-sm font-semibold text-gray-900 truncate hover:text-[#359BA2]">
+				{bookTitle}
+			</h2>
 
-      {/* Price */}
-      <div className="mt-2 text-xs">
-        {/* Discounted Price */}
-        {discountedPrice && (
-          <span className="text-[#FF5F00] font-semibold mr-1">Rs. {discountedPrice}</span>
-        )}
-        {/* Original Price */}
-        <span
-          className={`${discountedPrice ? "line-through text-gray-400" : "text-[#2B2B2B] font-semibold"}`}
-        >
-          Rs. {price}
-        </span>
-      </div>
+			{/* Rating */}
+			<div className="flex items-center text-xs text-yellow-500 mt-1">
+				⭐ {bookRating}
+				<span className="text-gray-500 ml-1">
+					({bookReviewCount} reviews)
+				</span>
+			</div>
 
-      {/* Action Buttons */}
-      <div className="mt-3 flex gap-1">
-        {/* Add to Cart Button */}
-        <button className="flex items-center justify-center basis-[70%] bg-[#359BA2] text-white text-xs py-1.5 rounded hover:bg-[#2c8a90] gap-1 transition-colors duration-300">
-          <FaCartPlus />
-          Add to cart
-        </button>
+			{/* Price */}
+			<div className="mt-2 text-xs">
+				{/* Discounted Price */}
+				{bookFinalPrice && (
+					<span className="text-[#FF5F00] font-semibold mr-1">
+						Rs. {bookFinalPrice}
+					</span>
+				)}
+				{/* Original Price */}
+				<span
+					className={`${
+						bookFinalPrice
+							? "line-through text-gray-400"
+							: "text-[#2B2B2B] font-semibold"
+					}`}
+				>
+					Rs. {bookPrice}
+				</span>
+			</div>
 
-        {/* Wishlist Button */}
-        <button className="flex items-center justify-center basis-[30%] bg-[#FF5F00] text-white text-xs py-1.5 rounded hover:bg-[#e55400] transition-colors duration-300">
-          <MdChecklistRtl />
-        </button>
-      </div>
-    </div>
-  );
+			{/* Action Buttons */}
+			<div className="mt-3 flex gap-1">
+				{/* Add to Cart Button */}
+				<button className="flex items-center justify-center basis-[70%] bg-[#359BA2] text-white text-xs py-1.5 rounded hover:bg-[#2c8a90] gap-1 transition-colors duration-300">
+					<FaCartPlus />
+					Add to cart
+				</button>
+
+				{/* Wishlist Button */}
+				<button className="flex items-center justify-center basis-[30%] bg-[#FF5F00] text-white text-xs py-1.5 rounded hover:bg-[#e55400] transition-colors duration-300">
+					<MdChecklistRtl />
+				</button>
+			</div>
+		</div>
+	);
 };
 
 export default ProductCard;
--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,101 +1,96 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import bgImage from "../assets/image.png";
+import { FaUser, FaLock } from "react-icons/fa"; // Importing icons from react-icons
+// import styles from "./LoginPage.module.css";
+import bgImage from "../assets/login.jpeg";
 import TextInput from "../components/TextInput";
-import { MdLockOutline } from "react-icons/md";
-import { CgProfile } from "react-icons/cg";
 
 const LoginPage = () => {
-  const [formData, setFormData] = useState({
-    email: "",
-    password: "",
-  });
+	const [formData, setFormData] = useState({
+		userName: "",
+		password: "",
+	});
 
-  const handleChange = (e) => {
-    const { id, value } = e.target;
-    setFormData({ ...formData, [id]: value });
-  };
+	const handleChange = (e) => {
+		const { id, value } = e.target;
+		setFormData({ ...formData, [id]: value });
+	};
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
+	const handleSubmit = async (e) => {
+		e.preventDefault();
 
-    try {
-      const response = await fetch(
-        "https://localhost:7240/api/Auth/login",
-        {
-          // const response = await fetch(`${process.env.REACT_APP_BOOKNEST_URL}/Auth/stafflogin`, {
-          method: "POST",
-          mode: "cors",
-          headers: { "Content-Type": "application/json" },
-          body: JSON.stringify(formData),
-        }
-      );
+		try {
+			const response = await fetch(
+				"https://localhost:7240/api/Auth/stafflogin",
+				{
+					method: "POST",
+					mode: "cors",
+					headers: { "Content-Type": "application/json" },
+					body: JSON.stringify(formData),
+				}
+			);
 
-      if (response.ok) {
-        const result = await response.json();
-        sessionStorage.setItem("booknestUser", "hehe");
-        alert("Login successful! 🎉");
-      } else {
-        const error = await response.json();
-        alert(`Login failed: ${error.message}`);
-      }
-    } catch (error) {
-      alert("An error occurred: " + error.message);
-    }
-  };
+			if (response.ok) {
+				alert("Login successful! 🎉");
+				// You can redirect user here if needed
+			} else {
+				const error = await response.json();
+				alert(
+					`Login failed: ${error.message || "Invalid credentials"}`
+				);
+			}
+		} catch (error) {
+			alert("An error occurred: " + error.message);
+		}
+	};
 
-  return (
-    <div className="flex justify-center items-center mt-10">
-      <div className="flex w-[900px] h-[500px] bg-white overflow-hidden shadow-[0_6px_20px_rgba(0,0,0,0.1)] rounded-[20px]">
-        <div className="flex-1 flex flex-col justify-center px-[30px] py-10">
-          <h2 className="text-[32px] font-bold text-center mb-2">
-            Welcome Back
-          </h2>
-          <p className="italic text-[#666] text-sm text-center mb-[30px]">
-            Login to Continue
-          </p>
-          <form onSubmit={handleSubmit}>
-            <div className="flex flex-col mb-5">
-              <TextInput
-                id={"email"}
-                type="email"
-                placeholder="Enter your Email"
-                onChange={handleChange}
-                icon={<CgProfile />}
-              />
-            </div>
-            <div className="flex flex-col mb-5">
-              <TextInput
-                id="password"
-                type="password"
-                placeholder="Enter your username"
-                onChange={handleChange}
-                icon={<MdLockOutline />}
-              />
-            </div>
-            <button
-              type="submit"
-              className="w-full bg-[#5f6060] text-white text-base cursor-pointer transition-[background-color] duration-[0.3s] ease-[ease] mt-2.5 p-3 rounded-lg border-[none] hover:bg-[#3b3b3b];
-"
-            >
-              Login
-            </button>
-          </form>
-          <div className="text-center text-sm text-[#555] mt-4">
-            Don't have an account?{" "}
-            <Link to="/signup" className="hover:underline font-bold">
-              Create Account
-            </Link>
-          </div>
-        </div>
+	return (
+		<div className={styles.loginContainer}>
+			<div className={styles.loginForm}>
+				<div className={styles.formSection}>
+					<h2 className={styles.loginTitle}>Welcome Back</h2>
+					<p className={styles.loginSubtitle}>Login to Continue</p>
+					<form onSubmit={handleSubmit}>
+						<div className={styles.formGroup}>
+							{/* <label htmlFor="userName">Username</label> */}
+							<TextInput
+								type="text"
+								id="userName"
+								placeholder="Enter your username"
+								required
+								onChange={handleChange}
+								icon={<FaUser />} // Using FaUser icon
+							/>
+						</div>
+						<div className={styles.formGroup}>
+							{/* <label htmlFor="password">Password</label> */}
+							<TextInput
+								type="password"
+								id="password"
+								placeholder="Enter your password"
+								required
+								onChange={handleChange}
+								icon={<FaLock />} // Using FaLock icon
+							/>
+						</div>
+						<button type="submit" className={styles.loginButton}>
+							Login
+						</button>
+					</form>
+					<div className={styles.accountText}>
+						Don't have an account?{" "}
+						<Link to="/signup">Create Account</Link>
+					</div>
+				</div>
 
-        <div
-          className="flex-1 bg-cover bg-center"
-          style={{ backgroundImage: `url(${bgImage})` }}
-        ></div>
-      </div>
-    </div>
-  );
+				<div
+					className={styles.imageSection}
+					style={{ backgroundImage: `url(${bgImage})` }}
+				></div>
+				<p>this is for test</p>
+			</div>
+		</div>
+	);
 };
 
 export default LoginPage;
